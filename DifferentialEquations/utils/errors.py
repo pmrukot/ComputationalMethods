e89037be9e5abba71922650f67105962a1220224
Sławomir Mucha <slawomir.mucha@skygate.pl>
2016-05-04 18:56:03 +0200

    Refactor error calculation utilities

--- DifferentialEquations/utils/errors.py
+++ DifferentialEquations/utils/errors.py
@@ -1,15 +1,8 @@
 
 def calculate_mean_square_error(x, f_y, f_i):
     n = len(x)
-    mean_squared_error = 0
-    for i in range(0, n):
-        mean_squared_error += (f_y[i] - f_i[i])**2
-    mean_squared_error /= n
-    return mean_squared_error
+    return sum((f_y[i] - f_i[i])**2 for i in range(0, n)) / n
 
 
 def calculate_max_error(x, f_y, f_i):
-    result = 0
-    for i in range(0, len(f_y)):
-        result = max(abs(f_y[i] - f_i[i]), result)
-    return result
+    return max(abs(f_y[i] - f_i[i]) for i in range(0, len(x)))
